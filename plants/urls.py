5f41af71927059b2e90b8c86396e453c3c855308
Elke Harmanny <ElkeHarmanny@outlook.com>
2022-01-02 13:53:56 +0100

    Add fontawesome, jquery and working search

--- plants/urls.py
+++ plants/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_plants, name='all_plants'),
-    path('<category>/', views.all_plants,),
+    path('', views.all_plants, name='all_plants')
 ] 
--- plants/views.py
+++ plants/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from .models import Plant, Category
-
+from django.db.models import Q
 
 def all_plants(request):
     """ Return the plants overview page """
 
-    categories = Category.objects.all()
     plants = Plant.objects.all()
+    categories = Category.objects.all()
 
-    if 'category' in request.GET:
-        selected_category = request.GET['category']
-        plants = Plant.objects.filter(category__name=selected_category)
-    
+    if request.GET : 
+        if 'category' in request.GET:
+            selected_category = request.GET['category']
+            plants = Plant.objects.filter(category__name=selected_category)
+        if 'query' in request.GET :
+            query = request.GET['query']
+            queries = Q(common_name__icontains=query) | Q(
+                botanical_name__icontains=query) | Q(description__icontains=query)
+            plants = Plant.objects.filter(queries)
+
     context = {
         'plants': plants,
         'categories': categories
