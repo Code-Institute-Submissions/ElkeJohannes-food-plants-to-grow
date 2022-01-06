2c47be47314af0e106f06fbe0d1650c442fafe2f
elkejohannes <elkeharmanny@outlook.com>
2022-01-06 14:56:20 +0100

    Make allauth working and add templates

--- food_plants_to_grow/settings.py
+++ food_plants_to_grow/settings.py
@@ -81,10 +81,23 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # `allauth` needs this from django
+                'django.template.context_processors.request',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'food_plants_to_grow.wsgi.application'
 
