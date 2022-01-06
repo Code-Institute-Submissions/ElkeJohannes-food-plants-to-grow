766ec63e591175e32427e4895b9bf6b3dd1e84d3
elkejohannes <elkeharmanny@outlook.com>
2022-01-06 15:26:51 +0100

    Fix allauth templates and add empty profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def profile(request):
+    """ Return the users profile page """
+
+    return render(request, 'accounts/profile.html')
