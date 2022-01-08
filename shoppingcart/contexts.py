7f34d72765006c1e58f9855c458468142b913e47
Elke Harmanny <ElkeHarmanny@outlook.com>
2022-01-08 14:28:10 +0100

    Add add_to_cart functionality

--- shoppingcart/contexts.py
+++ shoppingcart/contexts.py
@@ -4,11 +4,16 @@ from django.shortcuts import get_object_or_404
 from plants.models import Plant
 
 def cart_contents(request):
-
     cart_items = []
     total_price = 0
     plant_count = 0
     cart = request.session.get('cart', {})
+
+    for key, value in cart.items():
+        plant = get_object_or_404(Plant, pk=key)
+        cart_items.append(plant)
+        total_price += plant.price * value
+        plant_count += value # or maybe just do a +1?
 
     context = {
         'cart_items': cart_items,
--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, reverse
+from django.shortcuts import get_object_or_404
+from plants.models import Plant
 
 
 def view_cart(request):
     """ Show the cart """
 
+    return render(request, 'shoppingcart/cart.html')
+
+
+def add_to_cart(request, plant_id):
+    """ Add an item to the cart """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    # If the plant is already in the cart, update the quantity
+    if plant_id in list(cart.keys()):
+        cart[plant_id] += quantity
+    else:
+        cart[plant_id] = quantity
+
+    request.session['cart'] = cart
+
+    # return redirect(reverse('all_plants'))
     return render(request, 'shoppingcart/cart.html')
